25138e96bbfe09102e2c5bb2aec188b08a0eafdc
Nicanope <nicole.motles@gmail.com>
2018-05-28 19:22:52 -0400

    Change style to class for visibility
    instead of adding a style to the element to adjust its visibility, I added a class to the image and manipulated the class with JavaScript.

--- Memory Game/app.js
+++ Memory Game/app.js
@@ -3,11 +3,10 @@ let cards = document.getElementsByClassName('card');
 for (let card of cards) {
     card.addEventListener('click', function() {
         let img = this.querySelector('.card img');
-        if (img.style.visibility === "hidden" || img.style.visibility === "") {
-            img.style.visibility = "visible"
+        if (img.className === "hidden") {
+            img.className = "visible"
         } else {
-            img.style.visibility = "hidden"
+            img.className = "hidden"
         }
     })
 }
-
