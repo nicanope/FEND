55482315fda79bddd1cbf867e140922a83ea2f28
Nicanope <nicole.motles@gmail.com>
2018-05-28 23:01:06 -0400

    Change names
    changed names so there were no capital letter on folder names

--- Memory Game/app.js
+++ Memory Game/app.js
@@ -1,12 +1,36 @@
-let cards = document.getElementsByClassName('card');
+// let count = 0;
+// let picture = document.querySelector('.card img')
 
-for (let card of cards) {
+// picture.onclick = function() {
+//     count++;
+// }
+
+// const cards = document.getElementsByClassName('card');
+
+// for (const card of cards) {
+//     card.addEventListener('click', function() {
+//         const img = this.querySelector('.card img');
+//         if (img.className === 'hidden' && count<3) {
+//             img.className = 'visible'
+//         } else {
+//             img.className = 'hidden'
+//         }
+//     })
+// }
+let count = 0;
+
+const cards = document.getElementsByClassName('card');
+
+for (const card of cards) {
     card.addEventListener('click', function() {
-        let img = this.querySelector('.card img');
-        if (img.className === "hidden") {
-            img.className = "visible"
+        const img = this.querySelector('.card img');
+        img.onclick = function() {
+            count++;
+        }
+        if (img.className === 'hidden' && count<3) {
+            img.className = 'visible'
         } else {
-            img.className = "hidden"
+            img.className = 'hidden'
         }
     })
 }
