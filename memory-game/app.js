06660b3ed545daeeb2c4c6afa4295653c1dfabe9
Nicanope <nicole.motles@gmail.com>
2018-05-29 03:26:07 -0400

    Clean up classes, style and app file
    Deleted old code from the app.js file and cleaned up classes and in-line style from the index

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,32 +1,12 @@
-// let count = 0;
-// let picture = document.querySelector('.card img')
-
-// picture.onclick = function() {
-//     count++;
-// }
-
-// const cards = document.getElementsByClassName('card');
-
-// for (const card of cards) {
-//     card.addEventListener('click', function() {
-//         const img = this.querySelector('.card img');
-//         if (img.className === 'hidden' && count<3) {
-//             img.className = 'visible'
-//         } else {
-//             img.className = 'hidden'
-//         }
-//     })
-// }
 let count = 0;
-
 const cards = document.getElementsByClassName('card');
+
+// This loop allows you to only flip two cards at a time.
 
 for (const card of cards) {
     card.addEventListener('click', function() {
         const img = this.querySelector('.card img');
-        img.onclick = function() {
-            count++;
-        }
+        count++;
         if (img.className === 'hidden' && count<3) {
             img.className = 'visible'
         } else {
@@ -34,3 +14,12 @@ for (const card of cards) {
         }
     })
 }
+// onclick.setTimeout (
+// for (const card of cards) {
+//     card.addEventListener('click', function() {
+//         const imgs = this.querySelectorAll('.card img');
+//         if (count => 3) {
+//             count = 0;
+//             imgs.className = 'hidden';
+//     }
+// }, 1)
