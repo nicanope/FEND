87e439e596607afdc88bda01db387bbd1101feea
Nicanope <nicole.motles@gmail.com>
2018-05-30 20:14:41 -0400

    Changed images to icons

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,10 +1,9 @@
 let count = 0;
 const cards = document.getElementsByClassName('card');
 
-// This loop allows you to only flip two cards at a time.
 
 for (const card of cards) {
-    card.addEventListener('click', function() {
+    card.addEventListener('click', function(e) {
         const img = this.querySelector('.card img');
         count++;
         if (img.className === 'hidden' && count<3) {
@@ -12,14 +11,39 @@ for (const card of cards) {
         } else {
             img.className = 'hidden'
         }
+
+
+
+        setTimeout(function () {
+            if (count => 3 && notMatched) {
+                count = 0;
+                imgs.className = 'hidden';
+            }
+        }, 1000)
     })
 }
-// onclick.setTimeout (
-// for (const card of cards) {
-//     card.addEventListener('click', function() {
-//         const imgs = this.querySelectorAll('.card img');
-//         if (count => 3) {
-//             count = 0;
-//             imgs.className = 'hidden';
-//     }
-// }, 1)
+
+// function to shuffle cards
+
+function shuffleCards(grid, table) {
+	table.innerHTML = '';
+	for(let i=0; i < grid.length; i++) {
+		let tr = document.createElement('tr');
+		for (let f=0; f<grid[i].length; f++) {
+			let td = document.createElement('td');
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+}
+
+
+let img = document.createElement('img')
+img.src = "some-url.com"
+td.appendChild(img)
+
+// function to check is cards match
+
+function checkMatch () {
+    
+}
