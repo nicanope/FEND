15d2f95b8a756bc1d5b80c8a814c049fc1d6ae33
Nicanope <nicole.motles@gmail.com>
2018-06-01 16:52:19 -0400

    Win the game
    When you match all the cards, it alerts you that you have won the game. Will now change the alert for a modal.

--- memory-game-master/js/app.js
+++ memory-game-master/js/app.js
@@ -13,31 +13,54 @@ const icons = [ 'fa-diamond',
 I concatenated the array to itself */
 const allIcons = icons.concat(icons);
 
-// this is the array that will hold the cards you've opened
+// array that will hold the cards you've opened
 let openCards = [];
-// this is just a variable that holds the whole deck for the event listener
+// variable that stores the whole deck for the event listener
 const deck = document.querySelector('.deck');
+// variable that holds the move count
+let moves = 0;
+// store the repeat button to restart the game
+const restartBtn = document.querySelector('.fa-repeat');
+// store the moves element in a variable
+const movesCounter = document.querySelector('.moves');
+// once the cards exist, this variable holds them
+const card = document.querySelectorAll('.card');
+// array that will hold all matched cards
+let matchedCards = [];
+
+let matchingCards = document.getElementsByClassName('match');
+
+// function called when the restart button is pressed
+// function restartGame(){
+//     moves = 0;
+//     // timer reset;
+//     // stars reset;
+//     // shuffle(allIcons);
+// createCards();
+// }
+
+// restartBtn.addEventListener('click', restartGame);
 
 // using the shuffle algorithm that was provided in the starter code
 shuffle(allIcons);
 
 // once the icons are shuffled, this loop generates the cards within the deck
-for (let icon of allIcons) {
-    let card = document.createElement('li');
-    card.innerHTML = `<li class="card hide"><i class="fa ${icon}"></i></li>`;
-    deck.appendChild(card);
+// ? Should I store this in a function to call it when I reset the game?
+function createCards(){    
+    for (let icon of allIcons) {
+        let card = document.createElement('li');
+        card.innerHTML = `<li class="card hide"><i class="fa ${icon}"></i></li>`;
+        deck.appendChild(card);
+    }
 }
 
-// once the cards exist, this variable holds them
-const card = document.querySelectorAll('.card');
-
+createCards();
 /* this function checks whether the cards opened match,
 then, if they do, turns them green and keeps them open,
 then clears the openCards array.
 If they don't match, it starts a timeout, and then flips
 the cards back and clears the openCards array.
 */
-
 function match(){
     const card1 = openCards[0].querySelector('i').className;
     const card2 = openCards[1].querySelector('i').className;
@@ -45,6 +68,8 @@ function match(){
     if (card1 === card2) {
         openCards[0].className = 'card match show';
         openCards[1].className = 'card match show';
+        matchedCards.push(card1);
+        matchedCards.push(card2);
         openCards = [];
     } else {
         setTimeout(function() {
@@ -53,33 +78,42 @@ function match(){
             openCards = [];
             console.log('in timeout');
         }, 500)
-    } 
+    } if (matchingCards.length == 16){
+        setTimeout(function() {
+            winner();
+        }, 500)
+    }
 }
 
 /* this function listens to the click within the deck
-with the card as the target, and turns the card once clicked. 
+with the card as the target, and turns the card once clicked.
+then it adds one to the move counter. 
 it specifies to only turn the card if it's hidden and there are
 less than two cards in the openCards array.
 if two cards are open, check if they match with the `match()`
 function */
-
 function turnCard(e){
     let clickedCard = e.target;
+    if (clickedCard.classList.contains('card')) {
+    moves++;
+    movesCounter.innerText = `${moves} moves`;
+    }
+    console.log(moves);
     console.log(openCards);
-    if (e.target.classList.contains('hide') && openCards.length<2) {
-        e.target.className = 'card open show';
+    if (clickedCard.classList.contains('hide') && openCards.length<2) {
+        clickedCard.className = 'card open show';
         openCards.push(clickedCard);
     } if (openCards.length == 2){
            match();
-        }
+    }
 }
 
+// event listener for the cards that triggers the function above
 deck.addEventListener('click', turnCard);
 
 
 /* Shuffle function from http://stackoverflow.com/a/2450976 
 provided with the starter code*/
-
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -90,12 +124,12 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-
-
+function winner(){
+    alert('You have won!');
+}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
